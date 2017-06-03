ac7ca52f8fbcde81498aebc9d910e3abe0547eb4
Peter Koch <peterept@gmail.com>
2017-06-03 14:10:45 +1000

    Adding project

---
+++ HelloCI/Assets/Colorer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Colorer : MonoBehaviour, IPointerClickHandler {
+
+	public void OnPointerClick(PointerEventData eventData)
+	{		
+		Debug.Log("Clicked" + name);        
+		GetComponent<Renderer> ().material.color = Random.ColorHSV (0, 1);
+	}
+}
